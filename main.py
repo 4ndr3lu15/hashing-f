60428d103b20d40ac5f117471a3157d28ab24067
Cachopa <andreluis@protonmail.com>
2022-09-16 20:03:23 -0300

    asdfasdfs

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ for i in range(len(lines)):
     lexico.append(a)
 
 H = table.HTable()
-H.take_f(fun.f2)
+H.take_f(fun.f3)
 H.put(lexico)
 # H.print_table()
 print(H.col_cont)
 print(H.meantime_put)
+
+
+print(H.get('vulgarizador'))
+print(H.meantime_get[0])
+print(H.get('vulgar'))
+print(H.meantime_get[0])
